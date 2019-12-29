7c8f07b9c98ac2ce8c9f667a3e5d64f756a6c79a
jsvelasquez <ba.back87@gmail.com>
2019-12-29 18:06:52 -0500

    se monta el requerimiento de ComprasCategoria

--- src/main/java/io/jsVelasquez/leonisa/Pedido.java
+++ src/main/java/io/jsVelasquez/leonisa/Pedido.java
@@ -1,24 +1,34 @@
 package io.jsVelasquez.leonisa;
 
+import com.sun.xml.internal.bind.v2.runtime.SchemaTypeTransducer;
+
 import java.util.ArrayList;
 
 public class Pedido {
     private int cantidadUnidad;
+    private String medioPago;
+    private double descuento;
+    private int prontoPago;
 
     private ArrayList<Producto> product = new ArrayList<Producto>();
+    private ArrayList<Producto> productoArr = new ArrayList<Producto>();
 
 
-    public Pedido() {
+    public Pedido(){
+
     }
 
-    public Pedido(int cantidadUnidad) {
+    public Pedido(int cantidadUnidad, String medioPago, double descuento, int prontoPago) {
         this.cantidadUnidad = cantidadUnidad;
 
     }
 
 
+
+
     public void asignarProducto(Leonisa leonisa){
         product.add(leonisa);
+
 
     }
 
@@ -30,6 +40,13 @@ public class Pedido {
     public void asignarProducto(Teen teen){
         product.add(teen);
 
+
+    }
+
+    public double totalCompra(double precio){
+        double totalCompra = 0;
+        totalCompra = precio * this.cantidadUnidad;
+        return  totalCompra;
     }
 
 }
--- src/test/java/io/jsVelasquez/leonisa/PedidoTest.java
+++ src/test/java/io/jsVelasquez/leonisa/PedidoTest.java
@@ -8,30 +8,34 @@ import static org.junit.Assert.*;
 
 
 public class PedidoTest {
-
+    private Pedido pedidoLeonisa;
+    private Pedido pedidoLeo;
+    private Pedido pedidoTeen;
     private Leonisa leonisa;
     private Leo leo;
     private Teen teen;
     private Pedido pedido;
 
+
     private ArrayList<Producto> product = new ArrayList<Producto>();
+
 
     @Before
     public void setUp() throws Exception {
 
-
+        pedidoLeonisa = new Pedido(8, "Efectivo", 0,0);
+        pedidoLeo = new Pedido(12, "Pago efectivo", 0,0);
+        pedidoTeen = new Pedido(6, "Pago efectivo", 0,0);
         leonisa = new Leonisa("jk002", 15000, "brasier", "34d", "blanco", "encaje");
         leo = new Leo("pk020", 7000, "boxer", "S", "azul", "poliester");
         teen = new Teen("jt003", 24000, "tanga", "S", "negro", "licra");
 
-        product.add(leonisa);
-        product.add(leo);
-        product.add(teen);
+
     }
 
     @Test
     public void asignarProducto() {
-        boolean asigLeonisa = product.contains(leonisa);
+        boolean asigLeonisa= product.contains(leonisa);
         Assert.assertTrue("No se ha asignado producto 'Leonisa' al pedido", asigLeonisa);
     }
 
@@ -46,4 +50,27 @@ public class PedidoTest {
         boolean asigTeen = product.contains(teen);
         Assert.assertTrue("No se ha asignado producto 'Teen' al pedido", asigTeen);
     }
+
+    @Test
+    public void asignarProductoTeenTest(){
+        boolean asignadoTeen = product.contains(teen);
+        Assert.assertTrue("No se ha asignado un producto 'Teen' al pedido", asignadoTeen);
+    }
+
+    @Test
+    public void totalCompraTestLeonisa(){
+        Assert.assertEquals("El total de la compra no es el esperado", 120000, pedidoLeonisa.totalCompra(leonisa.getPrecio()), 0);
+    }
+
+    @Test
+    public void totalCompraTestLeo(){
+        Assert.assertEquals("El total de la compra no es el esperado", 84000, pedidoLeo.totalCompra(leo.getPrecio()), 0);
+    }
+
+    @Test
+    public void totalCompraTestTeen(){
+        Assert.assertEquals("El total de la compra no es el esperado", 144000, pedidoTeen.totalCompra(teen.getPrecio()), 0);
+    }
+
+
 }
